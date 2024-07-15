8f8823da0542dd94af99f052213c431188fc609d
fabiotrisera <fabio.trisera@gmail.com>
2024-07-16 07:39:30 +1000

    chore: Update HTML class in index.html and add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
-import { MdDarkMode } from "react-icons/md"
 import "./App.css"
-import { FaGithub, FaLinkedin } from "react-icons/fa"
 import Headline from "./components/Headline"
 import WorkExperience from "./components/WorkExperiences"
 import ThemeToggler from "./components/ThemeToggler"
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
       <ThemeToggler className="absolute top-3 right-3" />
       <div className="flex justify-between md:px-24 md:gap-4">
-        <header className="md:sticky md:top-0 md:w-1/2 md:max-h-screen md:py-20 text-center md:text-left sticky top-0">
-          <h1 className="font-bold dark:text-slate-100">Fabio Trisera</h1>
-          <h3 className="text-slate-900 dark:text-slate-100">
-            Senior Software Engineer
-          </h3>
-          <h4 className="text-slate-700 dark:text-slate-400 mt-6">
-            Full-Stack Development
-          </h4>
-          <ul>
-            <li>
-              <a href="#headline">Headline</a>
-            </li>
-            <li>
-              <a href="#work-experiences">Work Experience</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-          </ul>
-          <div className="flex mt-6 gap-x-4 justify-center md:justify-start">
-            <FaGithub className="size-8" />
-            <FaLinkedin className="size-8" />
-          </div>
-        </header>
+        <Header />
         <div className="md:flex-grow-1 md:w-1/2 md:py-20 flex flex-col">
           <Headline className="mb-12" />
           <WorkExperience className="mb-12" />
--- src/components/Keyword.tsx
+++ src/components/Keyword.tsx
@@ -1,7 +1,8 @@
 function Keyword({ children }: { children: React.ReactNode }): JSX.Element {
     return (
         <>
-            <span className="dark:text-slate-300 font-semibold">
+            <span className="dark:text-slate-300 font-semibold
+                    dark:hover:text-blue-500 hover:text-sky-400">
                 {children}
             </span>
         </>
